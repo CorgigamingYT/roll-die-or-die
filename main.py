64fefe56498c7dbf95b24a87e377a69f78f104c4
CorgigamingYT <43857110+CorgigamingYT@users.noreply.github.com>
2019-07-04 18:25:43 +0100

    making sure the code works

--- main.py
+++ main.py
@@ -2,64 +2,59 @@ import random
 import time
 
 
-def p1(text):
-    print(text)
+def is_even(num):
+    return num % 2 == 0
 
 
-def p2(text, score):
-    print(text)
-    print(int(score))
+def is_odd(num):
+    return not is_even(num)
 
 
-def p3(text, score, score2):
-    print(text)
-    print(int(score))
-    print(int(score2))
+def die_roll():
+    return random.randint(1, 6)
 
 
-def playround(round):
-
-    dice1roll1 = random.randint(1, 6)
-    dice2roll1 = random.randint(1, 6)
-    even = [2, 4, 6, 8, 10, 12]
-    odd = [1, 3, 5, 7, 9, 11]
+def playround(round, score):
+    dice_roll1 = die_roll()
+    dice_roll2 = die_roll()
     print("ROUND ", round)
     time.sleep(1)
-    p3("Your first numbers are", dice1roll1, dice2roll1)
-    points = dice1roll1 + dice2roll1
+    print("Your first numbers are " + str(dice_roll1) + " " + str(dice_roll2))
     time.sleep(1)
-    score1 = sum = float(dice1roll1) + float(dice2roll1)
-    if score1 in even:
-        p1("+10 points")
-        points = points + 10
+    score = score + dice_roll1 + dice_roll2
+    if is_even(score):
+        print("Your total after that round is even so have 10 points")
+        score += 10
     else:
-        p1("-5 points")
-        points = points - 5
-    if points <= 0:
-        points = + 1
-        p1("Have a point. On us.")
+        print("Your total after that round is odd so we will take away 5 points. Haahaa.")
+        score -= 5
+    if dice_roll1 == dice_roll2:
+        die_roll3 = die_roll()
+        print("You had 2 of the same value adding on another random number:  ." + str(die_roll3))
+        score = score + die_roll3
+    if score <= 0:
+        score = 1
+        print("Your points went below zero, setting points back to 1.")
     time.sleep(1)
-    print("Your score after round ", round,  "is:", points)
+    print("Your score after round ", round,  "is:", score)
     nr = input("To play next round, press y ").lower()
     if nr == 'y':
-
-        playround(round+1)
-    playround(round+1)
+        playround(round+1, score)
+    playround(round+1, score)
 
 
 user = input("Please enter a username: ")
-if user == "yeet":
+if user == "connor":
     password = input("Please enter a password: ")
-    if password == "yoot":
-        p1("Hello " + user)
+    if password == "austin":
+        print("Hello " + user)
         time.sleep(1)
-        p1("Do you want to start? ")
+        print("Do you want to start? ")
         time.sleep(1)
         print("""
-1. One Player
-2. Two Player
-3. Exit
+1. Play
+2. Exit
 """)
         yn = input("Please choose: ")
         if yn == "1":
-            playround(1)
+            playround(1, 0)
